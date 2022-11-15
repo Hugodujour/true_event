8a179a381468b28d15ce591d54bf4bb8244a6f8b
Hugodujour <hugogreg@live.fr>
2022-11-15 19:33:06 +0100

    fini

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :attendances
   resources :events
   resources :users
+  get 'static_pages', to:'static_pages#index' 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
